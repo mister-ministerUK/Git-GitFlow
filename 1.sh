d8b29bcdfefaaad7aa7276860747c2ebf013a146
Oleksandr Tserkovnyuk <tserkovnyuk.oleksandr@chnu.edu.ua>
2022-07-20 15:55:23 +0300

    Secondbr edit

--- 1.sh
+++ 1.sh
@@ -4,3 +4,5 @@ mkdir -p TSE/{ZAA/GR1,ZBB/GR2,ZCC}
 
 printf ": Summer2022\n: 20.07.2022\n: tserkovnyuk.oleksandr@chnu.edu.ua" > TSE/ZAA/GR1/Oleksandr.txt
 printf ": Summer2022\n: 20.07.2022\n: tserkovnyuk.oleksandr@chnu.edu.ua" > TSE/ZBB/GR2/Tserkovnyuk.txt 
+
+echo "Edit"
